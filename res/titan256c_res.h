340fbf51f8e75cb926a9a212afec68a0be9791cf
Pablo Fabricio Lettieri <fabri1983@gmail.com>
2024-01-17 19:53:02 -0300

    New rescomp_ext.jar

--- res/titan256c_res.h
+++ res/titan256c_res.h
@@ -1,6 +1,15 @@
 #ifndef _RES_TITAN256C_RES_H_
 #define _RES_TITAN256C_RES_H_
 
+#define COMPER 4
+#define KOSINSKI 5
+#define KOSINSKI_PLUS 6
+
+typedef struct
+{
+	u16 *tilemap;
+} TileMapCustom;
+
 typedef struct
 {
 	TileSet *tileset;
@@ -11,8 +20,8 @@ typedef struct
 {
 	TileSet *tileset1;
 	TileSet *tileset2;
-	TileMap *tilemap1;
-	TileMap *tilemap2;
+	TileMapCustom *tilemap1;
+	TileMapCustom *tilemap2;
 } ImageNoPalsTilesetSplit2;
 
 typedef struct
@@ -20,15 +29,28 @@ typedef struct
 	TileSet *tileset1;
 	TileSet *tileset2;
 	TileSet *tileset3;
-	TileMap *tilemap1;
-	TileMap *tilemap2;
-	TileMap *tilemap3;
+	TileMapCustom *tilemap1;
+	TileMapCustom *tilemap2;
+	TileMapCustom *tilemap3;
 } ImageNoPalsTilesetSplit3;
 
 typedef struct
 {
 	u16* data;
 } Palette32AllStrips;
+
+typedef struct
+{
+	u16* data1;
+	u16* data2;
+} Palette32AllStripsSplit2;
+
+typedef struct
+{
+	u16* data1;
+	u16* data2;
+	u16* data3;
+} Palette32AllStripsSplit3;
 
 extern const ImageNoPals titanRGB;
 extern const Palette32AllStrips palTitanRGB;
